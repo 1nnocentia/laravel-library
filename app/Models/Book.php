5c5ba1b6eab36edb116075b8079b281c789103bd
inno <innocentiahan14@gmail.com>
2025-11-18 11:50:29 +0800

    create model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    
+    protected $fillable = [
+        'isbn',
+        'title',
+        'author',
+        'published_year',
+        'stock',
+    ];
 }
